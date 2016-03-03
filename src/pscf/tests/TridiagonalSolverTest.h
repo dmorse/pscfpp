88ea1c50da0bc3f67743c9969a96fa0fbcb6860b
David Morse <morse012@umn.edu>
2016-03-03 10:24:21 -0600

    Finished testing general TridiagonalSolver (passed)

--- src/pscf/tests/TridiagonalSolverTest.h
+++ src/pscf/tests/TridiagonalSolverTest.h
@@ -30,7 +30,7 @@ public:
       solver.allocate(3);
    }
 
-   void testDecompose()
+   void testSymmetricDecompose()
    {
       printMethod(TEST_FUNC);
       TridiagonalSolver solver;
@@ -47,7 +47,7 @@ public:
       solver.computeLU(d, u);
    }
 
-   void testMultiply()
+   void testSymmetricMultiply()
    {
       printMethod(TEST_FUNC);
       TridiagonalSolver solver;
@@ -93,7 +93,7 @@ public:
       TEST_ASSERT(eq(x[2], 4.0));
    }
 
-   void testSolve()
+   void testSymmetricSolve()
    {
       printMethod(TEST_FUNC);
       TridiagonalSolver solver;
@@ -123,10 +123,119 @@ public:
       TEST_ASSERT(eq(b[1], y[1]));
       TEST_ASSERT(eq(b[2], y[2]));
    }
+
+   void testDecompose()
+   {
+      printMethod(TEST_FUNC);
+      TridiagonalSolver solver;
+      solver.allocate(3);
+
+      DArray<double> d, u, l;
+      d.allocate(3);
+      u.allocate(2);
+      l.allocate(2);
+      d[0] = 1.0;
+      d[1] = 3.0;
+      d[2] = 4.0;
+      u[0] = 2.0;
+      u[1] = 5.0;
+      l[0] = 1.0;
+      l[1] = -3.0;
+      solver.computeLU(d, u, l);
+   }
+
+   void testMultiply()
+   {
+      printMethod(TEST_FUNC);
+      TridiagonalSolver solver;
+      solver.allocate(3);
+
+      DArray<double> d, u, l;
+      d.allocate(3);
+      u.allocate(2);
+      l.allocate(2);
+      d[0] = 1.0;
+      d[1] = 3.0;
+      d[2] = 4.0;
+      u[0] = 2.0;
+      u[1] = 5.0;
+      l[0] = 1.0;
+      l[1] = -3.0;
+      solver.computeLU(d, u, l);
+ 
+      DArray<double> b, x;
+      b.allocate(3);
+      x.allocate(3);
+      // std::cout << "\n";
+      b[0] = 1.0;
+      b[1] = 0.0;
+      b[2] = 0.0;
+      solver.multiply(b, x);
+      // std::cout << x[0] << "  " << x[1] << "  " << x[2] << "\n";
+      TEST_ASSERT(eq(x[0], 1.0));
+      TEST_ASSERT(eq(x[1], 1.0));
+      TEST_ASSERT(eq(x[2], 0.0));
+      b[0] = 0.0;
+      b[1] = 1.0;
+      b[2] = 0.0;
+      solver.multiply(b, x);
+      // std::cout << x[0] << "  " << x[1] << "  " << x[2] << "\n";
+      TEST_ASSERT(eq(x[0], 2.0));
+      TEST_ASSERT(eq(x[1], 3.0));
+      TEST_ASSERT(eq(x[2], -3.0));
+      b[0] = 0.0;
+      b[1] = 0.0;
+      b[2] = 1.0;
+      solver.multiply(b, x);
+      // std::cout << x[0] << "  " << x[1] << "  " << x[2] << "\n";
+      TEST_ASSERT(eq(x[0], 0.0));
+      TEST_ASSERT(eq(x[1], 5.0));
+      TEST_ASSERT(eq(x[2], 4.0));
+   }
+
+   void testSolve()
+   {
+      printMethod(TEST_FUNC);
+      TridiagonalSolver solver;
+      solver.allocate(3);
+
+      DArray<double> d, u, l;
+      d.allocate(3);
+      u.allocate(2);
+      l.allocate(2);
+      d[0] = 1.0;
+      d[1] = 3.0;
+      d[2] = 4.0;
+      u[0] = 2.0;
+      u[1] = 5.0;
+      l[0] = 1.0;
+      l[1] = -3.0;
+      solver.computeLU(d, u, l);
+ 
+      DArray<double> b, x, y;
+      b.allocate(3);
+      x.allocate(3);
+      b[0] = 1.0;
+      b[1] = 2.0;
+      b[2] = 3.0;
+      solver.solve(b, x);
+      y.allocate(3);
+      solver.multiply(x, y);
+      // std::cout << y[0] << "  " << y[1] << "  " << y[2] << "\n";
+      TEST_ASSERT(eq(b[0], y[0]));
+      TEST_ASSERT(eq(b[1], y[1]));
+      TEST_ASSERT(eq(b[2], y[2]));
+   }
+   #if 0
+   #endif
+
 };
 
 TEST_BEGIN(TridiagonalSolverTest)
 TEST_ADD(TridiagonalSolverTest, testConstructor)
+TEST_ADD(TridiagonalSolverTest, testSymmetricDecompose)
+TEST_ADD(TridiagonalSolverTest, testSymmetricMultiply)
+TEST_ADD(TridiagonalSolverTest, testSymmetricSolve)
 TEST_ADD(TridiagonalSolverTest, testDecompose)
 TEST_ADD(TridiagonalSolverTest, testMultiply)
 TEST_ADD(TridiagonalSolverTest, testSolve)
