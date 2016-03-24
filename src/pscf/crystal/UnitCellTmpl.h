feff9d404f0173aa6f64defbc9a73ff638b2e734
David Morse <morse012@umn.edu>
2016-03-24 11:46:30 -0500

    Drafts of RealVec and UnitCellTmpl

---
+++ src/pscf/crystal/UnitCellTmpl.h
@@ -0,0 +1,100 @@
+#ifndef PSCF_UNIT_CELL_TMPL_H
+#define PSCF_UNIT_CELL_TMPL_H
+
+/*
+* PSCF - Polymer Self-Consistent Field Theory
+*
+* Copyright 2016, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include <util/containers/FArray.h>
+#include <pscf/math/RealVec.h>
+#include <pscf/math/IntVec.h>
+
+namespace Pscf
+{ 
+
+   using namespace Util;
+
+   /**
+   * Template for a class representing a solvent species.
+   *
+   * \ingroup Pscf_Base_Module
+   */
+   template <int D>
+   class UnitCellTmpl 
+   {
+   public:
+
+      /**
+      * Constructor.
+      */
+      UnitCellTmpl()
+      {}
+   
+      /**
+      * Destructor.
+      */
+      ~UnitCellTmpl()
+      {}
+   
+      /**
+      * Get a Bravais basis vector i.
+      */
+      const RealVec<D>& rBasisVector(int i) const;
+   
+      /**
+      * Get reciprocal basis vector i.
+      */
+      const RealVec<D>& kBasisVector(int i) const;
+   
+      /**
+      * Get square magnitude of reciprocal basis vector.
+      */
+      virtual double ksq(IntVec<D> const & k) const;
+   
+   protected:
+  
+      FArray<RealVec<D>, D> rBasis_;
+
+      FArray<RealVec<D>, D> kBasis_;
+   
+   };
+
+   /*
+   * Get a Bravais basis vector i.
+   */
+   template <int D>
+   const RealVec<D>& UnitCellTmpl<D>::rBasisVector(int i) const
+   {  return rBasis_[i];  }
+
+   /*
+   * Get reciprocal basis vector i.
+   */
+   template <int D>
+   inline
+   const RealVec<D>& UnitCellTmpl<D>::kBasisVector(int i) const
+   {  return kBasis_[i];  }
+
+   /*
+   * Get square magnitude of reciprocal basis vector.
+   */
+   template <int D>
+   double UnitCellTmp<D>::ksq(IntVec<D> const & k) const
+   {
+      RealVec<D> g(0);
+      RealVec<D> p;
+      for (int i = 0; i < D; ++i) {
+         p.multiply(kBasis_[i], k[i]);
+         g =+ p;
+      }
+      double value = 0.0;
+      for (int i = 0; i < D; ++i) {
+         value += g[i]*g[i];
+      }
+      return value;
+   }
+   
+} 
+#endif 
--- src/pscf/math/RealVec.h
+++ src/pscf/math/RealVec.h
@@ -1,5 +1,5 @@
-#ifndef PSCF_INT_VEC_H
-#define PSCF_INT_VEC_H
+#ifndef PSCF_REAL_VEC_H
+#define PSCF_REAL_VEC_H
 
 /*
 * PSCF - Polymer Self-Consistent Field Theory
@@ -9,6 +9,7 @@
 */
 
 #include "Vec.h"
+
 #include <iostream>
 #include <util/global.h>
 
@@ -16,10 +17,10 @@ namespace Pscf
 {
 
    /**
-   * A IntVec<D, T> is D-component vector with elements of integer type.
+   * A RealVec<D, T> is D-component vector with elements of floating type T.
    */
-   template <int D, typename T = int>
-   class IntVec : public Vec<D, T>
+   template <int D, typename T = double>
+   class RealVec : public Vec<D, T>
    {
 
    public:
@@ -30,16 +31,16 @@ namespace Pscf
       /**
       * Default constructor
       */
-      IntVec<D, T>()
+      RealVec<D, T>()
         : Vec<D, T>()
       {}
 
       /**
       * Copy constructor
       *
-      * \param v IntVec<D, T> to be copied
+      * \param v RealVec<D, T> to be copied
       */
-      IntVec<D, T>(const IntVec<D, T>& v)
+      RealVec<D, T>(const RealVec<D, T>& v)
        : Vec<D, T>(v)
       {}
 
@@ -48,7 +49,7 @@ namespace Pscf
       *
       * \param v C array to be copied
       */
-      IntVec<D, T>(T const * v)
+      RealVec<D, T>(T const * v)
        : Vec<D, T>(v)
       {}
 
@@ -57,7 +58,7 @@ namespace Pscf
       *
       * \param s scalar initial value for all elements.
       */
-      explicit IntVec<D, T>(T s)
+      explicit RealVec<D, T>(T s)
        : Vec<D, T>(s)
       {}
 
@@ -66,21 +67,24 @@ namespace Pscf
       /// Width of field per Cartesian coordinate in stream IO
       static const int Width = 25;
 
+      /// Precision in stream IO of RealVec<D, T> coordinates
+      static const int Precision = 17;
+
    };
 
    // Friend functions and operators
-   
+
    /**
-   * istream extractor for a IntVec<D, T>.
+   * istream extractor for a RealVec<D, T>.
    *
    * Input elements of a vector from stream, without line breaks.
    *
    * \param in  input stream
-   * \param vector  IntVec<D, T> to be read from stream
+   * \param vector  RealVec<D, T> to be read from stream
    * \return modified input stream
    */
    template <int D, typename T>
-   std::istream& operator>>(std::istream& in, IntVec<D, T> &vector)
+   std::istream& operator>>(std::istream& in, RealVec<D, T> &vector)
    {
       for (int i = 0; i < D; ++i) {
          in >> vector[i];
@@ -89,51 +93,24 @@ namespace Pscf
    }
 
    /**
-   * ostream inserter for a IntVec<D, T>.
+   * ostream inserter for a RealVec<D, T>.
    *
-   * Output a IntVec<D, T> to an ostream, without line breaks.
+   * Output a RealVec<D, T> to an ostream, without line breaks.
    *
    * Output elements of a vector to stream, without line breaks.
    * \param  out  output stream
-   * \param  vector  IntVec<D, T> to be written to stream
+   * \param  vector  RealVec<D, T> to be written to stream
    * \return modified output stream
    */
    template <int D, typename T>
-   std::ostream& operator<<(std::ostream& out, const IntVec<D, T> &vector) 
+   std::ostream& operator<<(std::ostream& out, const RealVec<D, T> &vector)
    {
       for (int i = 0; i < D; ++i) {
-         out.width(IntVec<D, T>::Width);
+         out.width(RealVec<D, T>::Width);
          out << vector[i];
       }
       return out;
    }
 
-   /**
-   * Equality of two IntVec<D, T>s.
-   *
-   * \return true if v1 == v2, false otherwise.
-   */
-   template <int D, typename T>
-   inline 
-   bool operator == (const IntVec<D, T>& v1, const IntVec<D, T>& v2) 
-   {
-      for (int i = 0; i < D; ++i) {
-         if (v1[i] != v2[i]) {
-            return false;
-         }
-      }
-      return true;
-   }
-   
-   /**
-   * Inequality of two IntVec<D, T>s.
-   *
-   * \return true if v1 != v2, false if v1 == v2.
-   */
-   template <int D, typename T>
-   inline
-   bool operator != (const IntVec<D, T>& v1, const IntVec<D, T>& v2) 
-   { return !(v1 == v2); }
-   
 }
 #endif
