d96967c3208223ab8f32e68889f5965dfe625fce
David Morse <morse012@umn.edu>
2019-08-25 11:46:29 -0500

    Correcting copyright years in files in src/pscf

--- src/pscf/crystal/SpaceGroup.h
+++ src/pscf/crystal/SpaceGroup.h
@@ -4,7 +4,7 @@
 /*
 * Simpatico - Simulation Package for Polymeric and Molecular Liquids
 *
-* Copyright 2010 - 2017, The Regents of the University of Minnesota
+* Copyright 2016 - 2019, The Regents of the University of Minnesota
 * Distributed under the terms of the GNU General Public License.
 */
 
