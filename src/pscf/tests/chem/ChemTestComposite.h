450d3830557d95e461f6248a1e035fa58a5df3b1
David Morse <morse012@umn.edu>
2021-11-03 21:12:21 -0500

    Removing SolventDescriptorTest

--- src/pscf/tests/chem/ChemTestComposite.h
+++ src/pscf/tests/chem/ChemTestComposite.h
@@ -6,13 +6,11 @@
 #include "MonomerTest.h"
 #include "VertexTest.h"
 #include "BlockDescriptorTest.h"
-#include "SolventDescriptorTest.h"
 
 TEST_COMPOSITE_BEGIN(ChemTestComposite)
 TEST_COMPOSITE_ADD_UNIT(MonomerTest);
 TEST_COMPOSITE_ADD_UNIT(VertexTest);
 TEST_COMPOSITE_ADD_UNIT(BlockDescriptorTest);
-TEST_COMPOSITE_ADD_UNIT(SolventDescriptorTest);
 TEST_COMPOSITE_END
 
 #endif
