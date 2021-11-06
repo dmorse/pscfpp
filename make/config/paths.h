67545ee837bf27c05f7a41e56de46c5d313e0d25
David Morse <morse012@umn.edu>
2021-11-06 00:44:24 -0500

    Adding file paths.h, generated by setup script

---
+++ make/config/paths.h
@@ -0,0 +1,16 @@
+#ifndef PSCF_PATHS_H
+#define PSCF_PATHS_H
+
+/**
+* \file  paths.h
+* \brief File containing absolute paths to data, for use in code and tests.
+*
+* These paths are set in by the "setup" script, which must be rerun if the
+* pscfpp directory tree is relocated.
+*/
+
+#define PSCF_ROOT_DIR PWD/
+#define PSCF_DATA_DIR PWD/data/
+#define PSCF_XMPL_DIR PWD/examples/
+
+#endif
