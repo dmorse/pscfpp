c7ba7df0a92f876f3c13005558e1606c8a4445e4
Ryan Collanton <ryancollanton@RPCMBP-2.local>
2021-11-19 22:14:29 -0600

    Fully implemented BasisFieldState Unit Test.

--- src/pspc/tests/sweep/BasisFieldStateTest.h
+++ src/pspc/tests/sweep/BasisFieldStateTest.h
@@ -43,53 +43,123 @@ public:
       openLogFile("out/testConstructor.log");
 
       System<3> system;
-      BasisFieldState<3> bfs(system);
+      BasisFieldState<3> bfs1(system);
+      BasisFieldState<3> bfs2;
 
-   }
-
-   void testAllocate()
-   {
-      // system.fileMaster().setInputPrefix(filePrefix());
-      // system.fileMaster().setOutputPrefix(filePrefix());
-
-      // std::ifstream in;
-      // openInputFile("in/diblock/bcc/param.flex", in);
-      // system.readParam(in);
-      // in.close();
-
-      // system.readWBasis("in/diblock/bcc/omega.ref");
    }
 
    void testRead()
    {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testRead.log");
+      
+      System<3> system;
+      BasisFieldState<3> bfs(system);
+      BFieldComparison comparison;
+   
+      // Setup system
+      BasisFieldStateTest::SetUpSystem(system);
+
+      // Read in file one way
+      system.readWBasis("in/diblock/bcc/omega.ref");
+      // Read in file another way
+      bfs.read("in/diblock/bcc/omega.ref");
+      // Compare
+      comparison.compare(bfs.fields(), system.wFields());
+      // Assert small difference
+      TEST_ASSERT(comparison.maxDiff() < 5.0e-7);
 
    }
 
    void testWrite()
    {
+      // Write tested with a read/write/read/comparison procedure
+      printMethod(TEST_FUNC);
+      openLogFile("out/testWrite.log");
 
+      System<3> system;
+      BasisFieldState<3> bfs1(system), bfs2(system);
+      BFieldComparison comparison;
+
+      // Setup system
+      BasisFieldStateTest::SetUpSystem(system);
+
+      // read, write, read
+      bfs1.read("in/diblock/bcc/omega.ref");
+      bfs1.write("out/testBasisFieldStateWrite.ref");
+      bfs2.read("out/testBasisFieldStateWrite.ref");
+
+      // compare
+      comparison.compare(bfs1.fields(),bfs2.fields());
+      // Assert small difference
+      TEST_ASSERT(comparison.maxDiff() < 5.0e-7);
    }
 
    void testGetSystemState()
    {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testGetSystemState.log");
 
+      System<3> system;
+      BasisFieldState<3> bfs(system);
+      BFieldComparison comparison;
+
+      // Setup system
+      BasisFieldStateTest::SetUpSystem(system);
+
+      // Read in state using system
+      system.readWBasis("in/diblock/bcc/omega.ref");
+      // get it using bfs
+      bfs.getSystemState();
+      // compare
+      comparison.compare(bfs.fields(),system.wFields());
+      // Assert small difference
+      TEST_ASSERT(comparison.maxDiff() < 5.0e-7);
    }
 
    void testSetSystemState()
    {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testSetSystemState.log");
 
+      System<3> system;
+      BasisFieldState<3> bfs(system);
+      BFieldComparison comparison;
+
+      // Setup system
+      BasisFieldStateTest::SetUpSystem(system);
+
+      // Read in state using bfs
+      bfs.read("in/diblock/bcc/omega.ref");
+      // set system state
+      bfs.setSystemState(true);
+      // compare
+      comparison.compare(bfs.fields(),system.wFields());
+      // Assert small difference
+      TEST_ASSERT(comparison.maxDiff() < 5.0e-7);
    }  
 
+   void SetUpSystem(System<3> & system)
+   {
+      system.fileMaster().setInputPrefix(filePrefix());
+      system.fileMaster().setOutputPrefix(filePrefix());
+      std::ifstream in;
+      openInputFile("in/diblock/bcc/param.flex", in);
+      system.readParam(in);
+      in.close();
+   }
+
 };
+
+
 
 TEST_BEGIN(BasisFieldStateTest)
 
 TEST_ADD(BasisFieldStateTest, testConstructor)
-// TEST_ADD(BasisFieldStateTest, testAllocate)
-// TEST_ADD(BasisFieldStateTest, testRead)
-// TEST_ADD(BasisFieldStateTest, testWrite)
-// TEST_ADD(BasisFieldStateTest, testGetSystemState)
-// TEST_ADD(BasisFieldStateTest, testSetSystemState)
+TEST_ADD(BasisFieldStateTest, testRead)
+TEST_ADD(BasisFieldStateTest, testWrite)
+TEST_ADD(BasisFieldStateTest, testGetSystemState)
+TEST_ADD(BasisFieldStateTest, testSetSystemState)
 
 TEST_END(BasisFieldStateTest)
 
