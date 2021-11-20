5f3bbe91ebb041c0e5e8cf8e79ad376cdc98c8e0
David Morse <morse012@umn.edu>
2021-11-19 21:43:02 -0600

    Cleaning up syntax of FieldComparisonTest::testRFieldComparison

--- src/pspc/tests/field/FieldComparisonTest.h
+++ src/pspc/tests/field/FieldComparisonTest.h
@@ -24,14 +24,18 @@ public:
    void tearDown() 
    {}
 
-   void testRFieldComparison()
+   void testRFieldComparison_1D()
    {
       printMethod(TEST_FUNC);
 
       RField<1> rf_0, rf_1;
       int n = 10;
-      rf_0.allocate(n);
-      rf_1.allocate(n);
+      IntVec<1> dimensions;
+      dimensions[0] = n;
+      rf_0.allocate(dimensions);
+      rf_1.allocate(dimensions);
+      int size = rf_0.capacity();
+      TEST_ASSERT(size == n);
       for (int i = 0; i < n; ++i) {
          rf_0[i] = 2.0;
          rf_1[i] = 2.001;
@@ -53,11 +57,47 @@ public:
 
    }
 
+   void testRFieldComparison_2D()
+   {
+      printMethod(TEST_FUNC);
+
+      RField<2> rf_0, rf_1;
+      int m = 5;
+      int n = 10;
+      IntVec<2> dimensions;
+      dimensions[0] = m;
+      dimensions[1] = n;
+      rf_0.allocate(dimensions);
+      rf_1.allocate(dimensions);
+      int size = rf_0.capacity();
+      TEST_ASSERT(size == m*n);
+      for (int i = 0; i < size; ++i) {
+         rf_0[i] = 2.0;
+         rf_1[i] = 2.001;
+      }
+      RFieldComparison<2> comparison;
+      comparison.compare(rf_0,  rf_1);
+      setVerbose(1);
+      if (verbose() > 0) {
+         std::cout << "\n";
+         std::cout << "MaxDiff = " 
+                   << Dbl(comparison.maxDiff(), 20, 12) << "\n";
+         std::cout << "RmsDiff = " 
+                   << Dbl(comparison.rmsDiff(), 20, 12) << "\n";
+      }
+      TEST_ASSERT(comparison.maxDiff() < 0.0011);
+      TEST_ASSERT(comparison.maxDiff() > 0.0009);
+      TEST_ASSERT(comparison.rmsDiff() < 0.0011);
+      TEST_ASSERT(comparison.rmsDiff() > 0.0009);
+
+   }
+
 };
 
 
 TEST_BEGIN(FieldComparisonTest)
-TEST_ADD(FieldComparisonTest, testRFieldComparison)
+TEST_ADD(FieldComparisonTest, testRFieldComparison_1D)
+TEST_ADD(FieldComparisonTest, testRFieldComparison_2D)
 TEST_END(FieldComparisonTest)
 
 #endif
