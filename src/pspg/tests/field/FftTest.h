c10de87518fba9967583dd5f48500e1cd4f6ec5d
rpcollanton <rpcollanton@gmail.com>
2022-02-03 15:40:43 -0600

    Working 1D, 2D, 3D cuda FFT tests.

--- src/pspg/tests/field/FftTest.h
+++ src/pspg/tests/field/FftTest.h
@@ -184,7 +184,7 @@ void FftTest::testTransform3D() {
       for (int j = 0; j < n2; j++) {
          for (int k = 0; k < n3; k++){
             rank = k + ((j + (i * n2)) * n3);
-            in[rank] = 1.0 + double(rank)/double(in.capacity());
+            in[rank] = 1.0 + double(rank)/double(n1*n2*n3);
          }
       }
    }
@@ -212,140 +212,11 @@ void FftTest::testTransform3D() {
 
 }
 
-#if 0
-void FftTest::testTransform2D() 
-{
-   printMethod(TEST_FUNC);
-   //printEndl();
-
-   int n1 = 3;
-   int n2 = 3;
-   IntVec<2> d;
-   d[0] = n1;
-   d[1] = n2;
-
-   FFT<2> v;
-   v.setup(d);
-
-   RField<2> in;
-   in.allocate(d);
-   int rank = 0;
-   double x, y, cx, sy;
-   double twoPi = 2.0*Constants::Pi;
-   for (int i = 0; i < n1; i++) {
-      x = twoPi*float(i)/float(n1); 
-      cx = cos(x);
-      for (int j = 0; j < n2; j++) {
-         y = twoPi*float(j)/float(n2); 
-         sy = sin(y);
-         rank = j + (i * n2);
-         in[rank] = 0.5 + 0.2*cx + 0.6*cx*cx - 0.1*sy + 0.3*cx*sy;
-      }
-   }
-
-   // Forward transform in -> out
-   RFieldDft<2> out;
-   out.allocate(d);
-   TEST_ASSERT(eq(in.capacity() / in.meshDimensions()[1],
-                  out.capacity() / (out.meshDimensions()[1]/2 + 1)));
-   v.forwardTransform(in, out);
-
-   #if 1
-   // Save a copy of out
-   RFieldDft<2> outCopy(out);
-   TEST_ASSERT(out.capacity() == outCopy.capacity());
-   for (int i = 0; i < out.capacity(); ++i) {
-      TEST_ASSERT(eq(out[i][0], outCopy[i][0]));
-      TEST_ASSERT(eq(out[i][1], outCopy[i][1]));
-   }
-   #endif
-
-   // Inverse transform out -> inCopy
-   RField<2> inCopy;
-   inCopy.allocate(d);
-   v.inverseTransform(out, inCopy);
-
-   // Check if in == inCopy
-   for (int i = 0; i < n1; i++) {
-      for (int j = 0; j < n2; j++) {
-         rank = j + (i * n2);
-         TEST_ASSERT(eq(in[rank], inCopy[rank]));
-      }
-   }
-
-   #if 0
-   // Check if out was not modified my inverseTransform
-   for (int i = 0; i < out.capacity(); ++i) {
-      TEST_ASSERT(eq(out[i][0], outCopy[i][0]));
-      TEST_ASSERT(eq(out[i][1], outCopy[i][1]));
-   }
-   #endif
-
-}
-#endif
-
-// void FftTest::testTransform3D() {
-//    printMethod(TEST_FUNC);
-//    //printEndl();
-
-//    int n1 = 3;
-//    int n2 = 3;
-//    int n3 = 3;
-//    IntVec<3> d;
-//    d[0] = n1;
-//    d[1] = n2;
-//    d[2] = n3;
-
-//    FFT<3> v;
-//    v.setup(d);
-
-//    RField<3> in;
-//    RFieldDft<3> out;
-//    in.allocate(d);
-//    out.allocate(d);
-
-//    TEST_ASSERT(eq(in.capacity() / in.meshDimensions()[2],
-//                   out.capacity() / (out.meshDimensions()[2]/2 + 1)));
-
-//    int rank = 0;
-//    for (int i = 0; i < n1; i++) {
-//       for (int j = 0; j < n2; j++) {
-//          for (int k = 0; k < n3; k++){
-//             rank = k + ((j + (i * n2)) * n3);
-//             in[rank] = 1.0 + double(rank)/double(in.capacity());
-//          }
-//       }
-//    }
-
-//    v.forwardTransform(in, out);
-//    RField<3> inCopy;
-//    inCopy.allocate(d);
-//    v.inverseTransform(out, inCopy);
-
-//    for (int i = 0; i < n1; i++) {
-//       for (int j = 0; j < n2; j++) {
-//          for (int k = 0; k < n3; k++){
-//             rank = k + ((j + (i * n1)) * n3);
-//             TEST_ASSERT(eq(in[rank], inCopy[rank]));
-//          }
-//       }
-//    }
-
-//    RFieldComparison<3> comparison;
-//    comparison.compare(in, inCopy);
-//    //std::cout << std::endl;
-//    //std::cout << "maxDiff = " 
-//    //          << Dbl(comparison.maxDiff(), 20, 13)
-//    //          << std::endl;
-//    TEST_ASSERT(comparison.maxDiff() < 1.0E-12);
-
-// }
-
 TEST_BEGIN(FftTest)
 TEST_ADD(FftTest, testConstructor)
 TEST_ADD(FftTest, testTransform1D)
-// TEST_ADD(FftTest, testTransform2D)
-// TEST_ADD(FftTest, testTransform3D)
+TEST_ADD(FftTest, testTransform2D)
+TEST_ADD(FftTest, testTransform3D)
 TEST_END(FftTest)
 
 #endif
