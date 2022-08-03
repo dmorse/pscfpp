21a5a6293f3c9ebccf4bf66dae29e22422ba15cb
David Morse <morse012@umn.edu>
2022-08-02 20:48:58 -0500

    Updating gpu unit test makefiles

--- src/pspg/tests/cuda/Test.cu
+++ src/pspg/tests/cuda/Test.cu
@@ -1,21 +1,21 @@
 #include <util/global.h>
 #include "CudaTestComposite.h"
 
+#include <util/param/BracketPolicy.h>
 #include <test/TestRunner.h>
 #include <test/CompositeTestRunner.h>
 
 int main(int argc, char* argv[])
 {
+   BracketPolicy::set(BracketPolicy::Optional);
    CudaTestComposite runner;
 
-   #if 0
    if (argc > 2) {
       UTIL_THROW("Too many arguments");
    }
    if (argc == 2) {
       runner.addFilePrefix(argv[1]);
    }
-   #endif
 
    runner.run();
 }
