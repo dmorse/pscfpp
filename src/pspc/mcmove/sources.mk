78681702ffb1231d59a5151fcdd821886c510e93
chen7545 <chen7545@umn.edu>
2023-06-08 20:03:06 -0500

    successful ftmc real move

--- src/pspc/mcmove/sources.mk
+++ src/pspc/mcmove/sources.mk
@@ -8,7 +8,6 @@ pspc_mcmove_= \
   pspc/mcmove/McSimulator.cpp \
   pspc/mcmove/RealMove.cpp 
   
->>>>>>> ftmc_realmove
 pspc_mcmove_SRCS=\
      $(addprefix $(SRC_DIR)/, $(pspc_mcmove_))
 pspc_mcmove_OBJS=\
