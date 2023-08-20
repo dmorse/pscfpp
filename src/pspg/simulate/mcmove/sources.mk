6f22af70884275d966d46ce2a65470c0da2f6bf6
chen7545 <chen7545@umn.edu>
2023-08-20 17:29:12 -0500

    Adding gpu McMoveFactory

--- src/pspg/simulate/mcmove/sources.mk
+++ src/pspg/simulate/mcmove/sources.mk
@@ -1,5 +1,7 @@
 pspg_simulate_mcmove_= \
   pspg/simulate/mcmove/McMove.cu \
+  pspg/simulate/mcmove/McMoveFactory.cu \
+  pspg/simulate/mcmove/McMoveManager.cu \
   
 pspg_simulate_mcmove_SRCS=\
      $(addprefix $(SRC_DIR)/, $(pspg_simulate_mcmove_))
