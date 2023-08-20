4f5566950f3959f3477f07e344a5859ef4ff728e
chen7545 <chen7545@umn.edu>
2023-08-20 18:34:39 -0500

    Adding TrajectoryReaderFactory

--- src/pspg/simulate/trajectory/TrajectoryReaderFactory.cu
+++ src/pspg/simulate/trajectory/TrajectoryReaderFactory.cu
@@ -1,43 +1,16 @@
-#ifndef PSPG_TRAJECTORY_READER_TPP
-#define PSPG_TRAJECTORY_READER_TPP
+/*
+* PSCF - Polymer Self-Consistent Field Theory
+*
+* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
 
-#include "TrajectoryReaderFactory.h"
-
-// Subclasses of ConfigIo
-#include "FieldConfigReader.h"
+#include "TrajectoryReaderFactory.tpp"
 
 namespace Pscf {
 namespace Pspg {
-
-   using namespace Util;
-
-   /*
-   * Constructor
-   */
-   template <int D>
-   TrajectoryReaderFactory<D>::TrajectoryReaderFactory(System<D>& system)
-    : sysPtr_(&system)
-   {}
-
-   /*
-   * Return a pointer to a instance of Trajectory subclass className.
-   */
-   template <int D>
-   TrajectoryReader<D>* 
-   TrajectoryReaderFactory<D>::factory(const std::string &className) const
-   {
-      TrajectoryReader<D> *ptr = 0;
-
-      // Try subfactories first
-      ptr = trySubfactories(className);
-      if (ptr) return ptr;
-
-      if (className == "FieldConfigReader") {
-        ptr = new FieldConfigReader<D>(*sysPtr_);
-      } 
-      return ptr;
-   }
-
+   template class TrajectoryReaderFactory<1>;
+   template class TrajectoryReaderFactory<2>;
+   template class TrajectoryReaderFactory<3>;
 }
 }
-#endif
