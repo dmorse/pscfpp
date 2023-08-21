99b6acd4a24650bac9e95f4dba21713566cf544e
chen7545 <chen7545@umn.edu>
2023-08-20 19:41:04 -0500

    Add gpu ftmc analyzer

--- src/pspg/simulate/sources.mk
+++ src/pspg/simulate/sources.mk
@@ -1,10 +1,12 @@
 # Include source files lists from subdirectories
 include $(SRC_DIR)/pspg/simulate/mcmove/sources.mk
 include $(SRC_DIR)/pspg/simulate/trajectory/sources.mk
+include $(SRC_DIR)/pspg/simulate/analyzer/sources.mk
 
 pspg_simulate_= \
   $(pspg_simulate_mcmove_) \
   $(pspg_trajectory_) \
+  $(pspg_simulate_analyzer_) \
   pspg/simulate/McSimulator.cu \
   pspg/simulate/McState.cu 
   
