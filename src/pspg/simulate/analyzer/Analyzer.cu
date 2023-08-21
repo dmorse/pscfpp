1d5eedd1325b3d6eb5112a8c8e959f0cad663eee
chen7545 <chen7545@umn.edu>
2023-08-20 19:35:46 -0500

    Add gpu ftmc analyzer

---
+++ src/pspg/simulate/analyzer/Analyzer.cu
@@ -0,0 +1,21 @@
+/*
+* PSCF - Polymer Self-Consistent Field Theory
+*
+* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "Analyzer.tpp"
+
+namespace Pscf {
+namespace Pspg {
+
+   template<> long Analyzer<1>::baseInterval = 0;
+   template<> long Analyzer<2>::baseInterval = 0;
+   template<> long Analyzer<3>::baseInterval = 0;
+
+   template class Analyzer<1>;
+   template class Analyzer<2>;
+   template class Analyzer<3>;
+}
+}
---
+++ src/pspg/simulate/analyzer/TrajectoryWriter.cu
@@ -0,0 +1,16 @@
+/*
+* PSCF - Polymer Self-Consistent Field Theory
+*
+* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "TrajectoryWriter.tpp"
+
+namespace Pscf {
+namespace Pspg {
+   template class TrajectoryWriter<1>;
+   template class TrajectoryWriter<2>;
+   template class TrajectoryWriter<3>;
+}
+}
