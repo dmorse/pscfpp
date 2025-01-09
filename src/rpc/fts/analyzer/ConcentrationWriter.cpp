c0f4c8753efd2f84f43cfee7128eac1144c213c5
chen7545 <chen7545@umn.edu>
2025-01-09 03:03:30 -0800

    Adding ConcentrationWriter analyzer

---
+++ src/rpc/fts/analyzer/ConcentrationWriter.cpp
@@ -0,0 +1,16 @@
+/*
+* PSCF - Polymer Self-Consistent Field Theory
+*
+* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Distributed under the terms of the GNU General Public License.
+*/
+
+#include "ConcentrationWriter.tpp"
+
+namespace Pscf {
+namespace Rpc {
+   template class ConcentrationWriter<1>;
+   template class ConcentrationWriter<2>;
+   template class ConcentrationWriter<3>;
+}
+}
