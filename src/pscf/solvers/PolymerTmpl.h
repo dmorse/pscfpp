32586a59e876b86333177451a56d9b42cd64e05b
David Morse <morse012@umn.edu>
2025-04-04 23:00:24 -0500

    Small changes to PolymerTmpl.h

--- src/pscf/solvers/PolymerTmpl.h
+++ src/pscf/solvers/PolymerTmpl.h
@@ -714,13 +714,13 @@ namespace Pscf
                   ib = pId[0];   // block identifier
                   io = pId[1];   // outgoing direction identifier
                   if (io == 0) {
-                     UTIL_CHECK(block(ib).vertexId(0) == is);
+                     UTIL_CHECK(edge(ib).vertexId(0) == is);
                      ii = 1;
                   } else {
-                     UTIL_CHECK(block(ib).vertexId(1) == is);
+                     UTIL_CHECK(edge(ib).vertexId(1) == is);
                      ii = 0;
                   }
-                  ir = block(ib).vertexId(ii);
+                  ir = edge(ib).vertexId(ii);
                   for (k = 0; k < n; ++k) {
                      path = sendPaths[is][k];
                      // Send unless just received along same bond
@@ -825,9 +825,9 @@ namespace Pscf
 
       // Check validity of ids owned by blocks
       for (ib = 0; ib < nBlock_; ++ib) {
-         UTIL_CHECK(block(ib).id() == ib);
-         iv0 = block(ib).vertexId(0);
-         iv1 = block(ib).vertexId(1);
+         UTIL_CHECK(edge(ib).id() == ib);
+         iv0 = edge(ib).vertexId(0);
+         iv1 = edge(ib).vertexId(1);
          UTIL_CHECK(iv0 != iv1);
          UTIL_CHECK(iv0 >= 0);
          UTIL_CHECK(iv0 < nVertex_);
@@ -847,7 +847,7 @@ namespace Pscf
             UTIL_CHECK(ib < nBlock_);
             UTIL_CHECK(id >= 0);
             UTIL_CHECK(id < 2);
-            UTIL_CHECK(block(ib).vertexId(id) == iv);
+            UTIL_CHECK(edge(ib).vertexId(id) == iv);
          }
       }
 
@@ -864,9 +864,9 @@ namespace Pscf
             UTIL_CHECK(id >= 0);
             UTIL_CHECK(id < 2);
             if (id == 0) {
-               UTIL_CHECK(block(ib).vertexId(1) == iv);
+               UTIL_CHECK(edge(ib).vertexId(1) == iv);
             } else {
-               UTIL_CHECK(block(ib).vertexId(0) == iv);
+               UTIL_CHECK(edge(ib).vertexId(0) == iv);
             }
          }
       }
@@ -881,7 +881,7 @@ namespace Pscf
                UTIL_CHECK(ib == -1);
                UTIL_CHECK(id == -1);
             } else {
-               UTIL_CHECK(block(ib).vertexId(id) == iv0);
+               UTIL_CHECK(edge(ib).vertexId(id) == iv0);
             }
          }
       }
