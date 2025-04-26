176ca451e9b30b7eb11c1ae650e89b4aac7e66e1
David Morse <morse012@umn.edu>
2025-04-26 15:29:25 -0500

    Changing copyright to 2015 (first commit) to 2025 (present)

--- src/prdc/cpu/CField.tpp
+++ src/prdc/cpu/CField.tpp
@@ -4,7 +4,7 @@
 /*
 * PSCF Package
 *
-* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Copyright 2015 - 2025, The Regents of the University of Minnesota
 * Distributed under the terms of the GNU General Public License.
 */
 
--- src/rpc/solvers/Mixture.cpp
+++ src/rpc/solvers/Mixture.cpp
@@ -1,7 +1,7 @@
 /*
 * PSCF - Mixture Self-Consistent Field Theory
 *
-* Copyright 2016 - 2022, The Regents of the University of Minnesota
+* Copyright 2015 - 2025, The Regents of the University of Minnesota
 * Distributed under the terms of the GNU General Public License.
 */
 
