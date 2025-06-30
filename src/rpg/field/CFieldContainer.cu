84a7399ef72d2958816fb7269d8de70d86f18878
David Morse <morse012@umn.edu>
2025-06-29 21:42:04 -0500

    Deriving Rpg::CFieldContainer from base class template

--- src/rpg/field/CFieldContainer.cu
+++ src/rpg/field/CFieldContainer.cu
@@ -5,15 +5,20 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "CFieldContainer.tpp"
+#include "CFieldContainer.h"
+#include <prdc/field/CFieldsReal.tpp>
 
 namespace Pscf {
-namespace Rpg
-{
-
-   template class CFieldContainer<1>;
-   template class CFieldContainer<2>;
-   template class CFieldContainer<3>;
-
-} // namespace Rpg
-} // namespace Pscf
+   namespace Prdc {
+      // Explicit instantiations of base class template
+      template class CFieldsReal<1, Cuda::RField<1>, Rpg::FieldIo<1> >;
+      template class CFieldsReal<2, Cuda::RField<2>, Rpg::FieldIo<2> >;
+      template class CFieldsReal<3, Cuda::RField<3>, Rpg::FieldIo<3> >;
+   } 
+   namespace Rpg {
+      // Explicit instantiations of this class
+      template class CFieldContainer<1>;
+      template class CFieldContainer<2>;
+      template class CFieldContainer<3>;
+   } 
+}
--- src/rpg/field/CFieldContainer.h
+++ src/rpg/field/CFieldContainer.h
@@ -8,228 +8,66 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include <prdc/cuda/RField.h>             // member template parameter
-#include <util/containers/DArray.h>       // member template
+#include <prdc/field/CFieldsReal.h>   // base class template
+#include <rpg/field/FieldIo.h>        // base class template parameter
+#include <prdc/cuda/RField.h>         // base class template parameter
 
 namespace Pscf {
 namespace Rpg {
 
    using namespace Util;
-   using namespace Pscf::Prdc;
-   using namespace Pscf::Prdc::Cuda;
+   using namespace Prdc;
+   using namespace Prdc::Cuda;
 
    /**
    * A list of c fields stored in both basis and r-grid format.
    *
-   * A CFieldContainer<D> contains representations of a list of nMonomer
-   * fields that are associated with different monomer types in two 
-   * different related formats:
-   * 
-   *  - A DArray of DArray<double> containers holds components of each
-   *    field in a symmetry-adapted Fourier expansion (i.e., in basis 
-   *    format). This is accessed by the basis() and basis(int) 
-   *    member functions.
-   *
-   *  - A DArray of RField<D> containers holds valus of each field on
-   *    the nodes of a regular grid. This is accessed by the rgrid()
-   *    and rgrid(int) member functions.
+   * This class is simply a named partial specialization of the base 
+   * class template Pscf::Prdc::CFieldsReal, designed for use with a GPU.
+   * Please see documentation of the base class for API documentation.
    *
    * \ingroup Rpg_Field_Module
    */
    template <int D>
-   class CFieldContainer 
+   class CFieldContainer : public CFieldsReal<D, RField<D>, FieldIo<D> >
    {
 
    public:
 
-      /**
-      * Constructor.
-      */
-      CFieldContainer();
+      /// Alias for base class
+      typedef CFieldsReal<D, RField<D>, FieldIo<D> >  Base;
 
-      /**
-      * Destructor.
-      */
-      ~CFieldContainer();
+      // Inherited public member functions
+      using Base::setFieldIo;
+      using Base::setNMonomer;
+      using Base::setWriteUnitCell;
+      using Base::allocateRGrid;
+      using Base::allocateBasis;
+      using Base::allocate;
+      using Base::basis;
+      using Base::rgrid;
+      using Base::isAllocatedRGrid;
+      using Base::isAllocatedBasis;
+      //using Base::hasData;
+      //using Base::isSymmetric;
 
-      /**
-      * Set stored value of nMonomer.
-      * 
-      * May only be called once.
-      *
-      * \param nMonomer number of monomer types.
-      */
-      void setNMonomer(int nMonomer);
+   protected:
 
-      /**
-      * Allocate or re-allocate memory for fields in rgrid format.
-      *
-      * \param dimensions  dimensions of spatial mesh
-      */
-      void allocateRGrid(IntVec<D> const & dimensions);
-
-      /**
-      * De-allocate fields in rgrid format.
-      */
-      void deallocateRGrid();
-
-      /**
-      * Allocate or re-allocate memory for fields in basis format.
-      *
-      * \param nBasis  number of basis functions 
-      */
-      void allocateBasis(int nBasis);
-
-      /**
-      * De-allocate fields in basis format.
-      */
-      void deallocateBasis();
-
-      /**
-      * Allocate memory for both r-grid and basis field formats.
-      *
-      * This function may only be called once.
-      *
-      * \param nMonomer  number of monomer types
-      * \param nBasis  number of basis functions 
-      * \param dimensions  dimensions of spatial mesh
-      */
-      void allocate(int nMonomer, int nBasis, IntVec<D> const & dimensions);
-
-      /**
-      * Get array of all fields in basis format (non-const).
-      */
-      DArray< DArray<double> > & basis()
-      {  return basis_; }
-
-      /**
-      * Get array of all fields in basis format (const)
-      *
-      * The array capacity is equal to the number of monomer types.
-      */
-      DArray< DArray<double> > const & basis() const
-      {  
-         UTIL_ASSERT(isAllocatedBasis_);
-         return basis_; 
-      }
-
-      /**
-      * Get the field for one monomer type in basis format (non-const).
-      *
-      * \param monomerId integer monomer type index (0, ... ,nMonomer-1)
-      */
-      DArray<double> & basis(int monomerId)
-      {  
-         UTIL_ASSERT(isAllocatedBasis_);
-         return basis_[monomerId]; 
-      }
-
-      /**
-      * Get the field for one monomer type in basis format (const)
-      *
-      * \param monomerId integer monomer type index (0, ... ,nMonomer-1)
-      */
-      DArray<double> const & basis(int monomerId) const
-      {  
-         UTIL_ASSERT(isAllocatedBasis_);
-         return basis_[monomerId]; 
-      }
-
-      /**
-      * Get array of all fields in r-grid format (non-const).
-      */
-      DArray< RField<D> > & rgrid()
-      {
-         UTIL_ASSERT(isAllocatedRGrid_);
-         return rgrid_; 
-      }
-
-      /**
-      * Get array of all fields in r-grid format (const).
-      */
-      DArray< RField<D> > const & rgrid() const
-      {  
-         UTIL_ASSERT(isAllocatedRGrid_);
-         return rgrid_; 
-      }
-
-      /**
-      * Get field for one monomer type in r-grid format (non-const)
-      *
-      * \param monomerId integer monomer type index (0,..,nMonomer-1)
-      */
-      RField<D> & rgrid(int monomerId)
-      {  
-         UTIL_ASSERT(isAllocatedRGrid_);
-         return rgrid_[monomerId]; 
-      }
-
-      /**
-      * Get field for one monomer type in r-grid format (const).
-      *
-      * \param monomerId integer monomer type index (0,..,nMonomer-1)
-      */
-      RField<D> const & rgrid(int monomerId) const
-      {  
-         UTIL_ASSERT(isAllocatedRGrid_);
-         return rgrid_[monomerId]; 
-      }
-
-      /**
-      * Has memory been allocated for fields in r-grid format?
-      */
-      bool isAllocatedRGrid() const
-      {  return isAllocatedRGrid_; }
-
-      /**
-      * Has memory been allocated for fields in basis format?
-      */
-      bool isAllocatedBasis() const
-      {  return isAllocatedBasis_; }
-
-   private:
-
-      /*
-      * Array of fields in symmetry-adapted basis format
-      *
-      * Element basis_[i] is an array that contains the components
-      * of the field associated with monomer i, in a symmetry-adapted
-      * Fourier basis expansion. 
-      */
-      DArray< DArray<double> > basis_;
-
-      /*
-      * Array of fields in real-space grid (r-grid) format
-      *
-      * Element basis_[i] is an RField<D> that contains values of the 
-      * field associated with monomer i on the nodes of a regular mesh.
-      */
-      DArray< RField<D> > rgrid_;
-
-      /*
-      * Number of monomer types.
-      */
-      int nMonomer_;
-
-      /*
-      * Has memory been allocated for fields in r-grid format?
-      */
-      bool isAllocatedRGrid_;
-
-      /*
-      * Has memory been allocated for fields in basis format?
-      */
-      bool isAllocatedBasis_;
+      using Base::fieldIo;
 
    };
 
-   #ifndef RPG_FIELD_CONTAINER_TPP
    // Suppress implicit instantiation
    extern template class CFieldContainer<1>;
    extern template class CFieldContainer<2>;
    extern template class CFieldContainer<3>;
-   #endif
 
 } // namespace Rpg
+namespace Prdc {
+   // Suppress implicit instantiation of base class
+   extern template class CFieldsReal<1, Cuda::RField<1>, Rpg::FieldIo<1> >;
+   extern template class CFieldsReal<2, Cuda::RField<2>, Rpg::FieldIo<2> >;
+   extern template class CFieldsReal<3, Cuda::RField<3>, Rpg::FieldIo<3> >;
+} 
 } // namespace Pscf
 #endif
