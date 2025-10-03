2d318c9db3ffecbe17f9d9212145caf22baebf3e
David Morse <morse012@umn.edu>
2025-10-02 19:23:51 -0500

    Draft of rpc/tests AmIteratorGridTest

---
+++ src/rpc/tests/system/AmIteratorGridTest.h
@@ -0,0 +1,574 @@
+#ifndef RPC_AM_ITERATOR_GRID_TEST_H
+#define RPC_AM_ITERATOR_GRID_TEST_H
+
+#include <test/UnitTest.h>
+#include <test/UnitTestRunner.h>
+
+#include <rpc/system/System.h>
+#include <prdc/cpu/RField.h>
+#include <prdc/crystal/BFieldComparison.h>
+#include <util/tests/LogFileUnitTest.h>
+
+#include <fstream>
+
+using namespace Util;
+using namespace Pscf;
+using namespace Pscf::Prdc;
+using namespace Pscf::Rpc;
+
+class AmIteratorGridTest : public LogFileUnitTest
+{
+
+public:
+
+   void setUp()
+   {  setVerbose(0); }
+
+   // Utility functions
+
+   template <int D>
+   void setupSystem(System<D>& system, std::string fname)
+   {
+      system.fileMaster().setInputPrefix(filePrefix());
+      system.fileMaster().setOutputPrefix(filePrefix());
+
+      std::ifstream in;
+      openInputFile(fname, in);
+      system.readParam(in);
+      in.close();
+   }
+
+   // Unit test functions
+
+   void testIterate1D_lam_rigid()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_rigid.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/diblock/lam/param.rigid_grid");
+
+      // Read w fields
+      system.w().readBasis("in/diblock/lam/omega.ref");
+
+      // Get reference field in basis format
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+     
+      // Iterate and output solution
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      }
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_rigid_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_rigid_c.bf");
+
+      // Compare result to original in basis format
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+      BFieldComparison comparison(1);
+      comparison.compare(b_wFields_check, b_wFields);
+      if (verbose() > 0) {
+         Log::file() << "\n";
+         Log::file() << "Max error = " << comparison.maxDiff() << "\n";
+      }
+      TEST_ASSERT(comparison.maxDiff() < 5.0E-7);
+      #endif
+   }
+
+   void testIterate1D_lam_flex()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_flex.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/diblock/lam/param.flex_grid"); 
+
+      system.w().readBasis("in/diblock/lam/omega.ref");
+
+      // Get reference field
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // Read input w-fields, iterate and output solution
+      system.w().readBasis("in/diblock/lam/omega.in");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_flex_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_flex_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 5.0E-7;
+      if (verbose() > 0 || diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+
+   }
+
+   void testIterate1D_lam_soln()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_soln.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/solution/lam/param.grid"); 
+
+      // Get reference field
+      system.w().readBasis("in/solution/lam/w.bf");
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // iterate and output solution
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_soln_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_soln_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon 
+      double diff = comparison.maxDiff();
+      double epsilon = 2.0E-6;
+      if (verbose() > 0 || diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+   }
+
+   void testIterate1D_lam_blend()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_blend.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/blend/lam/param.closed_grid"); 
+
+      // Get reference field
+      system.w().readBasis("in/blend/lam/w.ref");
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // Read input w-fields, iterate and output solution
+      system.w().readBasis("in/blend/lam/w.bf");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_blend_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_blend_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+      double diff = comparison.maxDiff();
+      double epsilon = 5.0E-7;
+      setVerbose(1);
+      if (verbose() > 0 || diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+   }
+
+   void testIterate1D_lam_open_soln()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_open_soln.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/solution/lam_open/param.grid"); 
+
+      // Get reference field
+      system.w().readBasis("in/solution/lam_open/w.ref");
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // Read input w-fields, iterate and output solution
+      system.w().readBasis("in/solution/lam_open/w.bf");
+
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_open_soln_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_open_soln_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      //double epsilon = 5.0E-7;
+      double epsilon = 6.0E-6;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+   }
+
+   void testIterate1D_lam_open_blend()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid1D_lam_open_blend.log");
+
+      System<1> system;
+      setupSystem<1>(system,"in/blend/lam/param.open_grid"); 
+
+      // Get reference field
+      system.w().readBasis("in/blend/lam/w.ref");
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // Read input w-fields, iterate and output solution
+      system.w().readBasis("in/blend/lam/w.bf");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid1D_lam_open_blend_w.bf");
+      system.c().writeBasis("out/testIterateGrid1D_lam_open_blend_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 5.0E-7;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+
+   }
+
+   void testIterate2D_hex_rigid()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid2D_hex_rigid.log");
+
+      System<2> system;
+      setupSystem<2>(system,"in/diblock/hex/param.rigid_grid"); 
+
+      // Read reference solution
+      system.w().readBasis("in/diblock/hex/omega.ref");
+
+      // Get reference field
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      // Read initial guess, iterate, output solution
+      // RPC tests start from the reference solution, 
+      // rather than a nearby solution, so I guess do that here too?
+      // system.w().readBasis("in/diblock/hex/omega.in");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid2D_hex_rigid_w.bf");
+      system.c().writeBasis("out/testIterateGrid2D_hex_rigid_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 5.0E-7;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+   }
+
+   void testIterate2D_hex_flex()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid2D_hex_flex.log");
+
+      System<2> system;
+      setupSystem<2>(system,"in/diblock/hex/param.flex_grid"); 
+
+      // Read reference solution (produced by Fortran code)
+      system.w().readBasis("in/diblock/hex/omega.ref");
+
+      // Get reference field
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      system.w().readBasis("in/diblock/hex/omega.in");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0 
+      system.w().writeBasis("out/testIterateGrid2D_hex_flex_w.bf");
+      system.c().writeBasis("out/testIterateGrid2D_hex_flex_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 8.0E-7;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+
+   }
+
+   void testIterate3D_bcc_rigid()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid3D_bcc_rigid.log");
+
+      System<3> system;
+      setupSystem<3>(system,"in/diblock/bcc/param.rigid_grid"); 
+
+      system.w().readBasis("in/diblock/bcc/omega.ref");
+
+      // Get reference field
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      system.w().readBasis("in/diblock/bcc/omega.in");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid3D_bcc_rigid_w.bf");
+      system.c().writeBasis("out/testIterateGrid3D_bcc_rigid_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 7.0E-7;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+
+   }
+
+   void testIterate3D_bcc_flex()
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateGrid3D_bcc_flex.log");
+
+      System<3> system;
+      setupSystem<3>(system,"in/diblock/bcc/param.flex_grid"); 
+
+      system.w().readBasis("in/diblock/bcc/omega.ref");
+
+      // Get reference field
+      DArray< DArray<double> > b_wFields_check;
+      b_wFields_check = system.w().basis();
+
+      system.w().readBasis("in/diblock/bcc/omega.in");
+      int error = system.iterate();
+      if (error) {
+         TEST_THROW("Iterator failed to converge.");
+      };
+      TEST_ASSERT(!error);
+
+      #if 0
+      system.w().writeBasis("out/testIterateGrid3D_bcc_flex_w.bf");
+      system.c().writeBasis("out/testIterateGrid3D_bcc_flex_c.bf");
+
+      // Get test result
+      DArray< DArray<double> > b_wFields;
+      b_wFields = system.w().basis();
+
+      // Compare result to original
+      BFieldComparison comparison (1);
+      comparison.compare(b_wFields_check, b_wFields);
+
+      // Compare difference to tolerance epsilon
+      double diff = comparison.maxDiff();
+      double epsilon = 5.0E-7;
+      if (diff > epsilon) {
+         Log::file() << "\n";
+         Log::file() << "Max diff = " << comparison.maxDiff() << "\n";
+         Log::file() << "Rms diff = " << comparison.rmsDiff() << "\n";
+         Log::file() << "epsilon  = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+      #endif
+
+   }
+
+   void testIterateWithMaskAndH() // test manual entry of mask and h fields
+   {
+      printMethod(TEST_FUNC);
+      openLogFile("out/testIterateWithMaskAndH.log");
+      
+      // Set up system
+      System<1> system;
+      system.fileMaster().setInputPrefix(filePrefix());
+      system.fileMaster().setOutputPrefix(filePrefix());
+      std::ifstream in;
+      openInputFile("in/maskAndH/param_grid", in);
+      system.readParam(in);
+      in.close();
+
+      // Read initial guess
+      system.w().readBasis("in/maskAndH/w.bf");
+
+      // Read in the mask and external fields from file
+      system.mask().setFieldIo(system.domain().fieldIo());
+      system.mask().allocateBasis(system.domain().basis().nBasis()); 
+      system.mask().allocateRGrid(system.domain().mesh().dimensions());
+      system.mask().readBasis("in/maskAndH/mask.bf");
+      TEST_ASSERT(eq(system.mask().phiTot(), 8.0951532073e-01));
+
+      system.h().setFieldIo(system.domain().fieldIo());
+      system.h().allocateBasis(system.domain().basis().nBasis());
+      system.h().allocateRGrid(system.domain().mesh().dimensions());
+      system.h().readBasis("in/maskAndH/h.bf");
+
+      // Run the solve function
+      system.iterate();
+
+      // Check converged field is correct by comparing to files in in/maskAndH
+      DArray< DArray<double> > wFieldsCheck; // Copy of reference field
+      UnitCell<1> unitCell;
+      unitCell = system.domain().unitCell();
+      system.domain().fieldIo().readFieldsBasis("in/maskAndH/w.ref", 
+                                                wFieldsCheck, unitCell);
+      BFieldComparison bComparison(0); // object to compare fields
+      bComparison.compare(system.w().basis(), wFieldsCheck);
+
+      double diff = bComparison.maxDiff();
+      double epsilon = 1.0E-5; 
+      if (verbose() > 0 || diff > epsilon) {
+         std::cout << "\n";
+         std::cout << "diff    = " << diff << "\n";
+         std::cout << "epsilon = " << epsilon << "\n";
+      }
+      TEST_ASSERT(diff < epsilon);
+   }
+
+};
+
+TEST_BEGIN(AmIteratorGridTest)
+TEST_ADD(AmIteratorGridTest, testIterate1D_lam_rigid)
+TEST_ADD(AmIteratorGridTest, testIterate1D_lam_flex)
+TEST_ADD(AmIteratorGridTest, testIterate1D_lam_soln)
+TEST_ADD(AmIteratorGridTest, testIterate1D_lam_blend)
+TEST_ADD(AmIteratorGridTest, testIterate1D_lam_open_blend)
+//TEST_ADD(AmIteratorGridTest, testIterate1D_lam_open_soln)
+TEST_ADD(AmIteratorGridTest, testIterate2D_hex_rigid)
+TEST_ADD(AmIteratorGridTest, testIterate2D_hex_flex)
+TEST_ADD(AmIteratorGridTest, testIterate3D_bcc_rigid)
+TEST_ADD(AmIteratorGridTest, testIterate3D_bcc_flex)
+//TEST_ADD(AmIteratorGridTest, testIterateWithMaskAndH)
+TEST_END(AmIteratorGridTest)
+
+#endif
--- src/rpc/tests/system/SystemTestComposite.h
+++ src/rpc/tests/system/SystemTestComposite.h
@@ -6,14 +6,14 @@
 // include the headers for individual tests
 #include "SystemTest.h"
 #include "AmIteratorBasisTest.h"
-//#include "AmIteratorGridTest.h"
+#include "AmIteratorGridTest.h"
 #include "BeadTest.h"
 #include "ThreadTest.h"
 
 TEST_COMPOSITE_BEGIN(SystemTestComposite)
 TEST_COMPOSITE_ADD_UNIT(SystemTest)
 TEST_COMPOSITE_ADD_UNIT(AmIteratorBasisTest)
-//TEST_COMPOSITE_ADD_UNIT(AmIteratorGridTest)
+TEST_COMPOSITE_ADD_UNIT(AmIteratorGridTest)
 TEST_COMPOSITE_ADD_UNIT(BeadTest)
 TEST_COMPOSITE_ADD_UNIT(ThreadTest)
 TEST_COMPOSITE_END
