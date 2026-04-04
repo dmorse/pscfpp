16ad94e067defe12f79a3e7c260adb86a53625fe
David Morse <morse012@umn.edu>
2026-04-04 00:52:47 -0500

    Clean up

--- src/rp/fts/analyzer/analyzer.mod
+++ src/rp/fts/analyzer/analyzer.mod
@@ -7,8 +7,8 @@ namespace Rp{
    *
    * Class templates that implement data output and analysis operations for 
    * partial saddle-point field theoretic simulation (PS-FTS) calculations.
-   * Instantiations of each of the templates defined in the Rp namespace are 
-   * used as base classes for analogous templats defined in the Rpc and Rpg
+   * Specializations of each templates defined in the Rp namespace are used
+   * as base classes for analogous templats defined in the Rpc and Rpg
    * program-level namespaces, to reduce code duplcation.
    *
    * \ingroup Rp_Fts_Module
--- src/rpc/scft/iterator/IteratorFactory.cpp
+++ src/rpc/scft/iterator/IteratorFactory.cpp
@@ -5,8 +5,54 @@
 * Distributed under the terms of the GNU General Public License.
 */
 
-#include "IteratorFactory.tpp"
+#include "IteratorFactory.h"  
 
+// Subclasses of Rpc::Iterator 
+#include "AmIteratorBasis.h"
+#include "AmIteratorGrid.h"
+
+namespace Pscf {
+namespace Rpc {
+
+   using namespace Util;
+
+   /*
+   * Constructor
+   */
+   template <int D>
+   IteratorFactory<D>::IteratorFactory(System<D>& system)
+    : sysPtr_(&system)
+   {}
+
+   /* 
+   * Return a pointer to a instance of Iterator subclass className.
+   */
+   template <int D>
+   Iterator<D>* IteratorFactory<D>::factory(const std::string &className) 
+   const
+   {
+      Iterator<D>* ptr = nullptr;
+
+      // Try subfactories first
+      ptr = trySubfactories(className);
+      if (ptr) return ptr;
+ 
+      // Try to match classname
+      if (className == "Iterator" || className == "AmIteratorBasis" 
+          || className == "AmIterator" ) {
+         ptr = new AmIteratorBasis<D>(*sysPtr_);
+      } else 
+      if (className == "AmIteratorGrid") {
+         ptr = new AmIteratorGrid<D>(*sysPtr_);
+      }
+
+      return ptr;
+   }
+
+}
+}
+
+// Explicit instantiation definitions
 namespace Pscf {
 namespace Rpc {
    template class IteratorFactory<1>;
