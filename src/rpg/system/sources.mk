4655e3fb2c61e4776a3eddd08b91d6441660609d
David Morse <morse012@umn.edu>
2026-07-19 20:12:41 -0500

    Removing old Types class files

--- src/rpg/system/sources.mk
+++ src/rpg/system/sources.mk
@@ -1,7 +1,6 @@
 rpg_system_= \
    rpg/system/System.cu \
-   rpg/system/SystemConstRef.cu \
-   rpg/system/Types.cu 
+   rpg/system/SystemConstRef.cu 
 
 rpg_system_OBJS=\
      $(addprefix $(BLD_DIR)/, $(rpg_system_:.cu=.o))
